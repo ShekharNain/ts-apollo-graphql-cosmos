89099611a72248a0e45ac9650544fd1c9fe3bf6b
shnain <shnain@microsoft.com>
2018-09-04 21:56:20 +0530

    User router added

---
+++ src/api/resources/user/user.restRouter.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { userController } from './user.controller';
+
+export const userRouter: Router = Router();
+
+// This act as a middleware and inject the userById inside request and pass the request down 
+userRouter.param('id', userController.findUserByParam);
+
+userRouter.route('/')
+    .get(userController.getAllUsers);
+
+userRouter.route('/:id')
+    .get(userController.getUserById);
+
+userRouter.all('*', (req, res) => {
+    res.json({noUser: true});
+})
